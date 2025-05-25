e1a4370a9ba9540532e95e2bfe7fb967a0bc3a97
mauricio-alves <mauricioalvesnutri@gmail.com>
2025-05-25 12:12:48 -0300

    responsivity done

--- src/script/index.ts
+++ src/script/index.ts
@@ -32,16 +32,21 @@ async function renderPage() {
     `;
 
     const navbar = document.querySelector(".navbar") as HTMLElement;
+    const mobileNavbar = document.querySelector(
+      ".mobileNavbar-content"
+    ) as HTMLElement;
     const bannerSection = document.querySelector(".banner") as HTMLElement;
 
-    if (navbar && bannerSection) {
+    if ((navbar || mobileNavbar) && bannerSection) {
       window.addEventListener("scroll", () => {
         const bannerHeight = bannerSection.getBoundingClientRect().height;
 
         if (window.scrollY > bannerHeight - 50) {
-          navbar.classList.add("scrolled");
+          navbar?.classList.add("scrolled");
+          mobileNavbar?.classList.add("scrolled");
         } else {
-          navbar.classList.remove("scrolled");
+          navbar?.classList.remove("scrolled");
+          mobileNavbar?.classList.remove("scrolled");
         }
       });
     }
